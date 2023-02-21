6c03afb2c7f4b0f7f0c98bcf6704670986a5998e
Nemes1sX <kazstonk@gmail.com>
2023-02-21 11:17:19 +0200

    Added form request and rules

---
+++ ChildrenGift/Models/FormRequest/GiftFormRequest.cs
@@ -0,0 +1,13 @@
+﻿using ChildrenGift.Models.FormRequest.Rules;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChildrenGift.Models.FormRequest
+{
+    public class GiftFormRequest
+    {
+        [StringLength(255, MinimumLength = 4)]
+        public string Name { get; set; }
+        [Required, ExistingChild]
+        public int ChildId { get; set; }
+    }
+}
