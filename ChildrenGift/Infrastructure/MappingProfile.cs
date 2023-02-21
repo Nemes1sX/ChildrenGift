2a16832e101a500ecfbde84328862ca3344bb620
Nemes1sX <kazstonk@gmail.com>
2023-02-21 15:27:49 +0200

    Added infrastructure

---
+++ ChildrenGift/Infrastructure/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ChildrenGift.Models.Dtos;
+using ChildrenGift.Models.Entities;
+
+namespace ChildrenGift.Infrastructure
+{
+    public class MappingProfile : Profile
+    { 
+        public MappingProfile()
+        {
+            CreateMap<Child, ChildDto>();
+            CreateMap<Gift, GiftDto>();
+        }
+    }
+}
