975e903e25175ba841c89d09f6e92f01a2379f87
Nemes1sX <kazstonk@gmail.com>
2023-02-21 11:16:31 +0200

    Added entities

---
+++ ChildrenGift/Models/Entities/Child.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ChildrenGift.Models.Entities
+{
+    public class Child : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Gift> Gifts { get; set; }
+    }
+}
---
+++ ChildrenGift/Models/Entities/Gift.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChildrenGift.Models.Entities
+{
+    public class Gift : Entity
+    {
+        public string Name { get; set; }
+        [ForeignKey("Child")]
+        [Column("ChildId")]
+        public int ChildId { get; set; }
+        public Child Child { get; set; }
+    }
+}
